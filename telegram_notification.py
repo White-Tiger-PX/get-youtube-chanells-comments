0bcfa9478976ae76cb62b8cf8e2382c04e941198
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-01-30 00:03:06 +0300

    Снизил уровень логирования Telegram до WARNING

--- telegram_notification.py
+++ telegram_notification.py
@@ -1,6 +1,41 @@
+import logging
+
 from telegram import Bot
 
 import config
+
+
+async def send_message_to_chat(
+    message,
+    main_logger,
+    pin_message=False,
+    mention_user=False,
+    parse_mode='HTML',
+    telegram_bot_token=config.telegram_bot_token,
+    chat_id=config.chat_id,
+    user_id=config.user_id
+):
+    logging.getLogger("telegram").setLevel(logging.WARNING)
+
+    logger = main_logger.getChild('telegram_notification')
+
+    try:
+        bot = Bot(token=telegram_bot_token)
+
+        if mention_user and user_id:
+            message = f"{message}[\\.](tg://user?id={user_id})"
+
+        sent_message = await bot.send_message(
+            chat_id=chat_id,
+            text=message,
+            parse_mode=parse_mode
+        )
+
+        if pin_message:
+            await bot.pin_chat_message(chat_id=chat_id, message_id=sent_message.message_id)
+
+    except Exception as e:
+        logger.error(f"Ошибка при отправке сообщения: {e}")
 
 
 async def send_message_to_thread(
@@ -14,6 +49,8 @@ async def send_message_to_thread(
     chat_id=config.chat_id,
     user_id=config.user_id
 ):
+    logging.getLogger("telegram").setLevel(logging.WARNING)
+
     logger = main_logger.getChild('telegram_notification')
 
     try:
@@ -29,39 +66,6 @@ async def send_message_to_thread(
             parse_mode=parse_mode
         )
 
-        # Если указано, что сообщение нужно закрепить
-        if pin_message:
-            await bot.pin_chat_message(chat_id=chat_id, message_id=sent_message.message_id)
-
-    except Exception as e:
-        logger.error(f"Ошибка при отправке сообщения: {e}")
-
-
-async def send_message_to_chat(
-    message,
-    main_logger,
-    pin_message=False,
-    mention_user=False,
-    parse_mode='HTML',
-    telegram_bot_token=config.telegram_bot_token,
-    chat_id=config.chat_id,
-    user_id=config.user_id
-):
-    logger = main_logger.getChild('telegram_notification')
-
-    try:
-        bot = Bot(token=telegram_bot_token)
-
-        if mention_user and user_id:
-            message = f"{message}[\\.](tg://user?id={user_id})"
-
-        sent_message = await bot.send_message(
-            chat_id=chat_id,
-            text=message,
-            parse_mode=parse_mode
-        )
-
-        # Если указано, что сообщение нужно закрепить
         if pin_message:
             await bot.pin_chat_message(chat_id=chat_id, message_id=sent_message.message_id)
 
