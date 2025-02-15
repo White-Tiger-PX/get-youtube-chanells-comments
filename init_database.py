d433a20c263336f15615f929f46588130f881ee2
White-Tiger-PX <191647395+White-Tiger-PX@users.noreply.github.com>
2025-02-15 05:41:04 +0300

    Использование контекстного оператора with для инициализации таблицы в БД

--- init_database.py
+++ init_database.py
@@ -5,7 +5,7 @@ import config
 from set_logger import set_logger
 
 
-def init_database(database_path, main_logger):
+def init_database(database_path: str, main_logger):
     """
     Инициализирует базу данных SQLite, создавая таблицу `comments`, если она не существует.
 
@@ -15,50 +15,41 @@ def init_database(database_path, main_logger):
     Args:
         database_path (str): Путь к файлу базы данных SQLite.
         main_logger (logging.Logger): Логгер.
-
-    Returns:
-        None
     """
+    logger = main_logger.getChild('init_database')
+    logger.info("Инициализация базы данных.")
+
     try:
-        logger = main_logger.getChild('init_database')
-        logger.info("Инициализация базы данных.")
+        with sqlite3.connect(database_path) as conn:
+            cursor = conn.cursor()
 
-        conn = sqlite3.connect(database_path)
-        cursor = conn.cursor()
+            cursor.execute('''
+                CREATE TABLE IF NOT EXISTS comments (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
 
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS comments (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    youtube_video_id TEXT NOT NULL,
+                    channel_name TEXT NOT NULL,
+                    channel_id TEXT NOT NULL,
 
-                youtube_video_id TEXT NOT NULL,
-                channel_name TEXT NOT NULL,
-                channel_id TEXT NOT NULL,
+                    comment_id TEXT NOT NULL,
+                    author TEXT NOT NULL,
+                    author_channel_id TEXT NOT NULL,
 
-                comment_id TEXT NOT NULL,
-                author TEXT NOT NULL,
-                author_channel_id TEXT NOT NULL,
+                    text TEXT NOT NULL,
+                    publish_date TEXT,
+                    updated_date TEXT,
+                    reply_to TEXT
+                )
+            ''')
 
-                text TEXT NOT NULL,
-                publish_date TEXT,
-                updated_date TEXT,
-                reply_to TEXT
-            )
-        ''')
+            conn.commit()
 
-        conn.commit()
-
-        logger.info("Инициализация базы данных завершена.")
+           logger.info("Инициализация базы данных завершена.")
     except Exception as err:
         logger.error("Ошибка при инициализации базы данных: %s", err)
-    finally:
-        cursor.close()
-        conn.close()
 
 
 if __name__ == "__main__":
     logger = set_logger(config.log_folder)
 
-    init_database(
-        database_path=config.database_path,
-        main_logger=logger
-    )
+    init_database(database_path=config.database_path, main_logger=logger)
